e363007b23ea4f654913c7eff38ac45f0366e372
Chris O'Donnell <cdo@256.work>
2018-08-22 00:48:29 +0100

    First graph-edit interface

---
+++ mdb_edit_graph.h
@@ -0,0 +1,10 @@
+#include "mdb_graph.h"
+
+MDB_SKETCH __stdcall MDB_StartSketch();
+MDB_NODE __stdcall MDB_SetSketchRoot(MDB_SKETCH sketch, MDB_NODE node);
+MDB_NODE __stdcall MDB_SketchNode(MDB_SKETCH sketch, MDB_NODETYPE type);
+void __stdcall MDB_AddLink(MDB_NODE src, MDB_LINKDESC link, MDB_NODE dst);
+void __stdcall MDB_DiscardSketchNode(MDB_NODE node);
+void __stdcall MDB_FinaliseSketch(MDB_SKETCH sketch);
+MDB_NODE __stdcall MDB_FinishSketch(MDB_SKETCH sketch);
+void __stdcall MDB_DeleteNode(MDB_NODE node);
