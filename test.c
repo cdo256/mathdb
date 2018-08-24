9a21c2e9e35ce3a3ceaefbe4244b0e0e93e0287d
Chris O'Donnell <cdo@256.work>
2018-08-24 02:08:41 +0100

    Add simpler test

--- test.c
+++ test.c
@@ -1,17 +1,27 @@
 #include "mdb_manage.h"
 #include "mdb_edit_graph.h"
+#include "mdb_get_error.h"
+#include <stdio.h>
 
-#define TEST_COUNT 0
+#define TEST_COUNT 2
 #define PASS 1
 #define FAIL 0
 
 #define NAME 0
 #define RUN 1
 
-int Test(int i, int fn, char* const* name) {
+int Test(int i, int fn, char** const name) {
     switch (i) {
-        case 0: { *name = "basic formation construction";
-            if (fn == NAME) return;
+        case 0: { *name = "create free";
+            if (fn == NAME) return PASS;
+            MDB_CreateGraph();
+            MDB_error e;
+            int ret = !MDB_GetError(&e);
+            MDB_FreeGraph();
+            return ret;
+        } return FAIL;
+        case 1: { *name = "basic formation construction";
+            if (fn == NAME) return PASS;
             MDB_CreateGraph();
             MDB_SKETCH sketch = MDB_StartSketch();
             MDB_NODE plus = MDB_CreateConst("+");
@@ -39,11 +49,12 @@ int Test(int i, int fn, char* const* name) {
 
 int main() {
     int failCount = 0;
-
-    for (int i = 0; i < testCount; i++) {
-        printf("Running test %d (%s): ... ", i, );
-        int result = Test(i,);
-        if (!reusult) failCount++;
+    char* s;
+    for (int i = 0; i < TEST_COUNT; i++) {
+        Test(i,NAME,&s);
+        int result = Test(i,RUN,&s);
+        printf("Running test %d (%s): ... ", i, s);
+        if (!result) failCount++;
         printf("%s\n", result ? "passed" : "failed");
     }
     if (failCount == 0)
